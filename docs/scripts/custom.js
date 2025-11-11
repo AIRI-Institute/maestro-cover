f66ee015fce46b0689e11b03c80ce632937cc0c8
itramb <60043400+itramb@users.noreply.github.com>
2025-11-11 18:16:09 +0300

    Add files via upload
    Onepager

---
+++ docs/scripts/custom.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('Custom element loaded!');
+    // Initialize Fancybox
+    Fancybox.bind('[data-fancybox]', {
+        // Custom options
+    });
+})
